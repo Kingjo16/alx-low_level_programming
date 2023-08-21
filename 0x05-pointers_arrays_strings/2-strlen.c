22302e5d6b36ff63ba39e4a229fd5ca6e8182550
kidjo16 <kidusyohannesred@gmail.com>
2023-08-21 07:59:37 -0700

    sterlin

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sterlen - this calculates the length 
+ * @s: it points to the input string
+ * Return: determines the lenght of the string
+ */
+
+	int _strlen(char *s)
+{
+	int lenght = 0;
+	while (*S != '\0')
+	{
+		length++;
+		s++;
+}
+return length;
+}
