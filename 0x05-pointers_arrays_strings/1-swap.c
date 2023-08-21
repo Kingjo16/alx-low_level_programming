456432407e1affb4993a292907a920e7c23eb8f2
kidjo16 <kidusyohannesred@gmail.com>
2023-08-21 07:48:51 -0700

    swap2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,13 +5,10 @@
  * swap_int - it swaps the integers the two integers values
  * @a: its just a Pointer to the first interger
  * @b: this one is a pointer to the second integer
- */
- 
-
- 			
- 		void swap_int(int *a, int *b)
+ */ 			
+void swap_int(int *a, int *b)
 {
-				int temp = *a;
-				*a = *b;
-				*b =temp;
+	int temp = *a;
+	*a = *b;
+	*b =temp;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 
 
-
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 
 #endif 
