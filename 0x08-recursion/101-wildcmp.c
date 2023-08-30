d3a9319c57db280c6e4ad3d1c344589f6925b268
kidjo16 <kidusyohannesred@gmail.com>
2023-08-30 04:34:09 -0700

    bettry 101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * wildcmp - This compares two strings; returns 1 for iden or 0
  * @s1: Normal string
@@ -7,18 +8,18 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-        if (*s2 == '\0' && *s1 == '\0')
-                return (1);
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
 
-        if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-                return (0);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
 
-        if (*s1 == *s2)
-                return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-        if (*s2 == '*')
-                return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s2 == ')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
-        return (0);
+	return (0);
 }
 
