49941ad40d36a22369f02cd3602b7ecbed44f318
kidjo16 <kidusyohannesred@gmail.com>
2023-08-30 04:09:33 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,11 +8,11 @@
  */
 int find_sqrt(int n, int guess)
 {
-    if (guess * guess == n)
-        return guess;
-    if (guess * guess > n)
-        return -1;
-    return find_sqrt(n, guess + 1);
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+			return (find_sqrt(n, guess + 1));
 }
 
 /**
@@ -22,9 +22,8 @@ int find_sqrt(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-
-    return find_sqrt(n, 1);
+	if (n < 0)
+		return (-1);
+greturn find_sqrt(n, 1);
 }
 
