43ee3b22c6064f68b71f7423eaa22e64292c2c8b
kidjo16 <kidusyohannesred@gmail.com>
2023-10-02 09:12:36 -0700

    TREAD Lightly, She is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that Reads a Text file and prints
+ * it to the POSIX standard output.
+ * @letters: Number of printed and REadable letters
+ * @filename: The File which will be reas and printed in this case
+ * which is Requiescat
+ * Return: Return Null if theere is no file to read
+ * if the write fails and returns fails it gives 0
+ * it returns the actua, number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ReadN, WriteN;
+	int fd;
+	char *Buf;
+
+	if (filename == NULL)
+		return (0);
+	Buf = malloc(letters);
+	if (Buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	ReadN = read(fd, Buf, letters);
+	WriteN = write(STDOUT_FILENO, Buf, ReadN);
+
+	if (fd == -1 || ReadN == -1 || WriteN == -1 || WriteN != ReadN)
+	{
+		free(Buf);
+		return (0);
+	}
+	free(Buf);
+	close(0);
+	return (WriteN);
+}
