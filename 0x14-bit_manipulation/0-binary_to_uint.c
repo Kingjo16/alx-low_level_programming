2becb035f524957676ed2c87a392619258c6fea0
Kidus <kingkidy50@gmail.com>
2023-10-26 04:28:22 -0700

    Binary to Unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,31 @@
 #include "main.h"
+/**
+ * binary_to_uint - this changes a binary number into a unit number.
+ * @b: the number who could be changed.
+ * Return: the outcome which is the unit number will be returned.
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int outcome;
+	unsigned int outcome, new;
 	int m;
 
 	if (!b)
 		return (0);
 
-	for (m = 0; b[m] != '\0'; m++)
-	{
-		if (b[m] != '0' && b[m] != '1')
-		{
-			return (0);
-		}
+	outcome = 0;
 
-		outcome = (outcome << 1) | (b[m] - '0');
+	for (m = 0; b[m]; m++)
+	{
+
+		if (b[m] != '0' && b[m] != '1')
+			return (0);
 	}
+		for (new = 1, outcome = 0, m--; m >= 0; m--, new *= 2)
+		{
+			if (b[m] == '1')
+				outcome += new;
+		}
 
 	return (outcome);
 }
-
-
