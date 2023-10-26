a850c834e1e2b8c2f13d2051a3c90a372fe1e1c1
Kidus <kingkidy50@gmail.com>
2023-10-26 09:13:05 -0700

    fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,12 +8,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int m;
-
-	if (index > 64)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	for (m = 1; index > 0; index--, m *= 2)
-		*n += m;
+	*n ^= (1 << index);
+
 	return (1);
 }
