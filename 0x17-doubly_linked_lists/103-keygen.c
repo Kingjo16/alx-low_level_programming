40f8c0a8b2ebd8a3167971eb219c55bbb26f9535
Kidus <kingkidy50@gmail.com>
2023-12-07 11:47:58 -0800

    advanced

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -18,30 +18,4 @@ int main(int argc, char *argv[])
 
 	username_length = strlen(argv[1]);
 	key[0] = charset[(username_length ^ 59) & 63];
-
-    for (loop_index = 0, sum_ascii = 0; loop_index < username_length; loop_index++)
-        sum_ascii += argv[1][loop_index];
-    key[1] = charset[(sum_ascii ^ 79) & 63];
-
-    for (loop_index = 0, loop_product = 1; loop_index < username_length; loop_index++)
-        loop_product *= argv[1][loop_index];
-    key[2] = charset[(loop_product ^ 85) & 63];
-
-    for (max_char = argv[1][0], loop_index = 0; loop_index < username_length; loop_index++)
-        if ((char)max_char <= argv[1][loop_index])
-            max_char = argv[1][loop_index];
-    srand(max_char ^ 14);
-    key[3] = charset[rand() & 63];
-
-    for (loop_sum = 0, loop_index = 0; loop_index < username_length; loop_index++)
-        loop_sum += argv[1][loop_index] * argv[1][loop_index];
-    key[4] = charset[(loop_sum ^ 239) & 63];
-
-    for (random_seed = 0, loop_index = 0; (char)loop_index < argv[1][0]; loop_index++)
-        random_seed = rand();
-    key[5] = charset[(random_seed ^ 229) & 63];
-
-    printf("%s\n", key);
-    return 0;
 }
-
