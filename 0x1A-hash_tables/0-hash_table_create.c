c168263d697525de6bfe49348b1a7be6806ccf4f
Kidus <kingkidy50@gmail.com>
2023-12-22 05:40:04 -0800

    hae table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table;
+	unsigned long int index;
+
+	h_table = malloc(sizeof(hash_table_t));
+	if (h_table == NULL)
+		return (NULL);
+
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (h_table->array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		h_table->array[index] = NULL;
+
+	return (h_table);
+}
