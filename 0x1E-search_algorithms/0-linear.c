fc024c1759f0ee10d4852506928aea82f556114f
Kidus <kingkidy50@gmail.com>
2024-04-11 07:05:23 -0700

    new add algo 0-liner.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - liner search which searches for a value in an array of int
+ * @array: FIrst elemnt in the array to SEARch.
+ * @size: elemnts in the array.
+ * @value: This is the value to search.
+ * Return: If the value is not present or the array is NULL, -1.
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t ind;
